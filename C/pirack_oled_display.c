0e4a8e567ce9f5b83926f945f8ae8301bad86c68
Martin A. Brooks <martin@hinterlands.org>
2023-04-14 19:11:57 +0100

    allman apparently

--- C/pirack_oled_display.c
+++ C/pirack_oled_display.c
@@ -8,22 +8,26 @@ Demo for ssd1306 i2c driver for Raspberry Pi
 #include <unistd.h>
 
 
-int main(void) {
+int main(void)
+{
   unsigned char symbol = 0;
   ssd1306_begin(SSD1306_SWITCHCAPVCC, SSD1306_I2C_ADDRESS); //LCD Screen initialization
-  if (i2cd < 0) {
+  if (i2cd < 0)
+  {
     printf("I2C device failed to open\r\n");
     return 0;
   }
   usleep(150 * 1000); //Short delay Ensure the normal response of the lower function
   FirstGetIpAddress(); //Get IP address
-  while (1) {
+  while (1)
+  {
     LCD_Display(symbol);
     sleep(1);
     sleep(1);
     sleep(1);
     symbol++;
-    if (symbol == 3) {
+    if (symbol == 3)
+    {
       symbol = 0;
     }
   }
