7132a2ddec505245ead897c9cfe8de8daf88453e
Paul Waring <paul@xk7.net>
2023-04-15 18:20:42 +0100

    Code tidy using VSCode Format Document

--- C/pirack_oled_display.c
+++ C/pirack_oled_display.c
@@ -7,18 +7,17 @@ Demo for ssd1306 i2c driver for Raspberry Pi
 #include "time.h"
 #include <unistd.h>
 
-
 int main(void)
 {
   unsigned char symbol = 0;
-  ssd1306_begin(SSD1306_SWITCHCAPVCC, SSD1306_I2C_ADDRESS); //LCD Screen initialization
+  ssd1306_begin(SSD1306_SWITCHCAPVCC, SSD1306_I2C_ADDRESS); // LCD Screen initialization
   if (i2cd < 0)
   {
     printf("I2C device failed to open\r\n");
     return 0;
   }
-  usleep(150 * 1000); //Short delay Ensure the normal response of the lower function
-  FirstGetIpAddress(); //Get IP address
+  usleep(150 * 1000);  // Short delay Ensure the normal response of the lower function
+  FirstGetIpAddress(); // Get IP address
   while (1)
   {
     LCD_Display(symbol);
